ae29389fcbf9db734444fc1859d7c7e64101eb71
Yuri <aoddrewiid@gmail.com>
2024-08-22 15:17:02 -0300

    refactor: updating states after checking if the API is browser compatible and removing the 'shouldShowOnboarding' state update when the text is empty

--- frontend/src/components/new-note-card.tsx
+++ frontend/src/components/new-note-card.tsx
@@ -23,10 +23,6 @@ export const NewNoteCard = ({ newNote }: NewNoteCardProps) => {
 
   const handleContentChanged = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setContent(event.target.value);
-
-    if (event.target.value === "") {
-      setShouldShowOnboarding(true);
-    }
   };
 
   const handleSaveNote = async (event: FormEvent) => {
@@ -42,9 +38,6 @@ export const NewNoteCard = ({ newNote }: NewNoteCardProps) => {
   };
 
   const handleStartRecording = () => {
-    setIsRecording(true);
-    setShouldShowOnboarding(false);
-
     const isSpeechRecognitionAPIAvailable =
       "SpeechRecognition" in window || "webkitSpeechRecognition" in window;
 
@@ -52,6 +45,9 @@ export const NewNoteCard = ({ newNote }: NewNoteCardProps) => {
       alert("Infelizmente seu navegador não suporta a API de gravação!");
       return;
     }
+
+    setIsRecording(true);
+    setShouldShowOnboarding(false);
 
     const SpeechRecognitionAPI =
       window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -101,7 +97,10 @@ export const NewNoteCard = ({ newNote }: NewNoteCardProps) => {
           onInteractOutside={handleCloseModal}
           className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:h-[60vh] w-full bg-slate-700 outline-none md:rounded-md flex flex-col overflow-hidden"
         >
-          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100 focus:ring-2 focus:ring-slate-500 outline-none">
+          <Dialog.Close
+            onClick={handleCloseModal}
+            className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100 focus:ring-2 focus:ring-slate-500 outline-none"
+          >
             <XIcon size={20} />
           </Dialog.Close>
 
